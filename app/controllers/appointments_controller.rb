a089fc92fb7f5515a312df11957bd7ae2d38bc86
halezmarie <teacherhaleymarie@gmail.com>
2021-04-15 23:54:40 -0500

    added appointments, patients, and doctors

---
+++ app/controllers/appointments_controller.rb
@@ -0,0 +1,8 @@
+class AppointmentsController < ApplicationController
+    
+def show
+    @appointment = Appointment.find_by(id: params[:id])
+    @patient = @appointment.patient
+    @doctor = @appointment.doctor
+    end
+end
