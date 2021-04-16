ada23268d36e977eeea45eb9ee3d29e7f77dcee9
halezmarie <teacherhaleymarie@gmail.com>
2021-04-16 00:29:58 -0500

    Done.

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -1,4 +1,5 @@
 class DoctorsController < ApplicationController
+    
     def show
       @doctor = Doctor.find_by(id: params[:id])
       @appointments = @doctor.appointments
--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,4 +1,5 @@
 class PatientsController < ApplicationController
+  
     def show
       @patient = Patient.find_by(id: params[:id])
       @appointments = @patient.appointments
